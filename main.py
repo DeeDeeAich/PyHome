c89364617f685e35c7dd8a2872aea1e808bc91c0
DeeDeeAich <32936305+DeeDeeAich@users.noreply.github.com>
2020-12-27 22:03:40 -0600

    Update main.py

--- main.py
+++ main.py
@@ -33,28 +33,41 @@ def update_time():
 time_label = ttk.Label(root, text=time.strftime("%I:%M:%S"))
 time_label.grid(row=0, column=2)
 update_time()
+
 # RSS Feed - improve later
 link_label = {}
-url = feedparser.parse("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
-for num in range(0, 6):
-    try:
-        label_text = url["entries"][num]["title_detail"]["value"]
-        ttk.Label(root, text=f"{label_text[0:70]}...", font=("Times New Roman", 12)).grid(sticky=N, ipadx=1)
-        label_text = url["entries"][num]["id"]
-        link_label[num] = ttk.Label(root, text=f"{label_text[0:70]}...", font=("Consolas", 10))
-        link_label[num].grid(sticky=N, ipadx=2)
-    except KeyError:
-        continue
-    except IndexError:
-        continue
+rss_feed_entry = ttk.Entry(root)
+def get_articles():
+    for num in range(0, 6):
+        try:
+            rss_notice.grid_forget()
+            rss_feed_entry.grid_forget()
+            submit_rss.grid_forget()
+            url = feedparser.parse(rss_feed_entry.get())
+            label_text = url["entries"][num]["title_detail"]["value"]
+            row_num1 = num + 1
+            ttk.Label(root, text=f"{label_text[0:70]}...", font=("Times New Roman", 12)).grid(row=row_num1, column=0)
+            label_text = url["entries"][num]["id"]
+            link_label[num] = ttk.Label(root, text=f"{label_text[0:70]}...", font=("Consolas", 10))
+            row_num2 = num + 3
+            link_label[num].grid(row=row_num2, column=0)
+        except KeyError:
+            continue
+        except IndexError:
+            continue
 
-link_label[0].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][0]["id"]))
-link_label[1].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][1]["id"]))
-link_label[2].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][2]["id"]))
-link_label[3].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][3]["id"]))
-link_label[4].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][4]["id"]))
-link_label[5].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][5]["id"]))
+    # link_label[0].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][0]["id"]))
+    # link_label[1].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][1]["id"]))
+    # link_label[2].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][2]["id"]))
+    # link_label[3].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][3]["id"]))
+    # link_label[4].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][4]["id"]))
+    # link_label[5].bind("<Button-1>", lambda e: webbrowser.open(url["entries"][5]["id"]))
 
+rss_notice = ttk.Label(root, text="Please enter an RSS Feed Link:")
+rss_notice.grid(column=0, row=1)
+rss_feed_entry.grid(column=0, row=2)
+submit_rss = ttk.Button(root, text="Submit", command=get_articles)
+submit_rss.grid(column=0, row=3)
 ttk.Label(root, text="").grid(sticky=N, row=16)
 
 # Verses grabber - edit later
@@ -88,11 +101,16 @@ def submit_weather():
     page = requests.get(url)
     soup = BeautifulSoup(page.content, "html.parser")
     
-    ttk.Label(root, text=soup.find("div", class_="BNeawe iBp4i AP7Wnd").text, font=("Times New Roman", 12)).grid(row=13, column=2)
-    ttk.Label(root, text=soup.find("div", class_="BNeawe tAd8D AP7Wnd").text, font=("Times New Roman", 12)).grid(row=14, column=2)
+    ttk.Label(root, text=soup.find("div", class_="kCrYT").text, font=("Nevis", 15)).grid(row=13, column=2)
+    ttk.Label(root, text=soup.find("div", class_="BNeawe iBp4i AP7Wnd").text, font=("Times New Roman", 14)).grid(row=14, column=2)
+    ttk.Label(root, text=soup.find("div", class_="BNeawe tAd8D AP7Wnd").text, font=("Times New Roman", 14)).grid(row=15, column=2)
+
+def enter_key_weather(event):
+    submit_weather()
 
 submit_weather_button = ttk.Button(root, text="Submit", command=submit_weather)
 submit_weather_button.grid(row=15, column=2)
+location.bind("<Return>", enter_key_weather)
 
 # To-do list - edit and document later
 checklist_items = ttk.Entry(root)
@@ -136,10 +154,10 @@ def submit_checklist():
         if check_variable[i].get():
             items[i].grid_forget()
 
-def enter_key(event):
+def enter_key_checklist(event):
     submit_checklist()
 
 add_item = ttk.Button(root, command=submit_checklist, text="Add Item").grid(row=2, column=2)
-checklist_items.bind("<Return>", enter_key)
+checklist_items.bind("<Return>", enter_key_checklist)
 
 root.mainloop()
